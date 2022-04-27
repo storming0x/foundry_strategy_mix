84737ca143cd58bb6497a27b318b5bf9dbbe49da
anticlimactic <9568756+anticlimactic@users.noreply.github.com>
2022-04-27 21:30:42 +1000

    chore: remove ds-test as it is already a dep in forge-std

--- src/test/utils/ExtendedTest.sol
+++ src/test/utils/ExtendedTest.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.12;
 
-import "ds-test/test.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract ExtendedDSTest is DSTest {
+contract ExtendedTest is Test {
     // solhint-disable-next-line
     function assertNeq(address a, address b) internal {
         if (a == b) {
